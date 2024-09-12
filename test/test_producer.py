7cf243568b8485e9f351bc7134ac23811c89f35a
Jaden Kodjo Miles <87667954+kodjomiles@users.noreply.github.com>
2024-09-12 16:11:04 +0000

    changed what was in test message (#19)

--- test/test_producer.py
+++ test/test_producer.py
@@ -7,7 +7,9 @@ class TestKafkaNotificationProducer(unittest.TestCase):
     def test_send_notification(self):
         producer = KafkaNotificationProducer(["localhost:9092"], "test_topic")
         try:
-            producer.send_notification({"test": "This is a test message"})
+            producer.send_notification(
+                {"test": "This is a test message from the test suite"}
+            )
         finally:
             producer.close()
 
